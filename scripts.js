de3102851611d10df7d553bad873314aeabade5f
Parenji <lor.parenti@gmail.com>
2024-06-10 19:17:46 +0200

    lobby gara 2

--- scripts.js
+++ scripts.js
@@ -1,33 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const classifiche = ['classifica1.json', 'classifica2.json', 'classifica3.json', 'classifica4.json', 'classifica5.json'];
-        
-    classifiche.forEach((classifica, index) => {
-        const timestamp = new Date().getTime(); // Ottieni il timestamp corrente
-        const urlWithTimestamp = `${classifica}?_=${timestamp}`; // Aggiungi il timestamp alla URL
+document.addEventListener("DOMContentLoaded", function () {
+  const classifiche = [
+    "classifica1.json",
+    "classifica2.json",
+    "classifica3.json",
+    "classifica4.json",
+    "classifica5.json",
+  ];
 
-        fetch(urlWithTimestamp)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Errore nel caricamento della classifica: ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                let container = document.getElementById(`classifica${index + 1}`);
-                let html = '';
-                
-                // Genera la sezione "Lobby" per gli indici da 1 a 4
-                if (index < 4) {
-                    html += `<h2>Lobby ${index + 1}</h2>`;
-                } else {
-                    // Genera la sezione "Classifica Generale" per l'indice 5
-                    html += `<h2>Classifica Generale</h2>`;
-                }
-                // Genera il pulsante per nascondere/mostrare le colonne
-                html += `<button id="toggleColumnsButton${index + 1}" class="toggleColumnsButton">Mostra colonne dei punteggi dettagliati</button>`;
-                html += `<div style="margin-bottom: 5px;"></div>`;
-                // Genera la tabella
-                html += `<div class="table-container"><table id="table${index + 1}"><thead><tr>
+  classifiche.forEach((classifica, index) => {
+    const timestamp = new Date().getTime(); // Ottieni il timestamp corrente
+    const urlWithTimestamp = `${classifica}?_=${timestamp}`; // Aggiungi il timestamp alla URL
+
+    fetch(urlWithTimestamp)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            "Errore nel caricamento della classifica: " + response.statusText
+          );
+        }
+        return response.json();
+      })
+      .then((data) => {
+        let container = document.getElementById(`classifica${index + 1}`);
+        let html = "";
+
+        // Genera la sezione "Lobby" per gli indici da 1 a 4
+        if (index < 4) {
+          html += `<h2>Lobby ${index + 1}</h2>`;
+        } else {
+          // Genera la sezione "Classifica Generale" per l'indice 5
+          html += `<h2>Classifica Generale</h2>`;
+        }
+        // Genera il pulsante per nascondere/mostrare le colonne
+        html += `<button id="toggleColumnsButton${
+          index + 1
+        }" class="toggleColumnsButton">Mostra colonne dei punteggi dettagliati</button>`;
+        html += `<div style="margin-bottom: 5px;"></div>`;
+        // Genera la tabella
+        html += `<div class="table-container"><table id="table${
+          index + 1
+        }"><thead><tr>
                             <th>Posizione</th>
                             <th>ID Pilota PSN</th>
                             <th>ID Pilota GT7</th>
@@ -57,62 +69,89 @@ document.addEventListener("DOMContentLoaded", function() {
                             <th class="totale">TOT. Redbull</th>
                             <th class="totalone">TOTALE</th>
                         </tr></thead><tbody>`;
-                
-                data.forEach(item => {
-                    html += `<tr>
-                                <td>${item.posizione || ''}</td>
-                                <td>${item.id_psn || ''}</td>
-                                <td>${item.id_gt7 || ''}</td>
-                                <td class="pole hidden">${item.pole_wg || ''}</td>
-                                <td class="gara hidden">${item.gara_wg || ''}</td>
-                                <td class="gv hidden">${item.gv_wg || ''}</td>
-                                <td class="totale">${item.tot_wg || ''}</td>
-                                <td class="pole hidden">${item.pole_atl || ''}</td>
-                                <td class="gara hidden">${item.gara_atl || ''}</td>
-                                <td class="gv hidden">${item.gv_atl || ''}</td>
-                                <td class="totale">${item.tot_atl || ''}</td>
-                                <td class="pole hidden">${item.pole_fuj || ''}</td>
-                                <td class="gara hidden">${item.gara_fuj || ''}</td>
-                                <td class="gv hidden">${item.gv_fuj || ''}</td>
-                                <td class="totale">${item.tot_fuj || ''}</td>
-                                <td class="pole hidden">${item.pole_aut || ''}</td>
-                                <td class="gara hidden">${item.gara_aut || ''}</td>
-                                <td class="gv hidden">${item.gv_aut || ''}</td>
-                                <td class="totale">${item.tot_aut || ''}</td>
-                                <td class="pole hidden">${item.pole_spa || ''}</td>
-                                <td class="gara hidden">${item.gara_spa || ''}</td>
-                                <td class="gv hidden">${item.gv_spa || ''}</td>
-                                <td class="totale">${item.tot_spa || ''}</td>
-                                <td class="pole hidden">${item.pole_rb || ''}</td>
-                                <td class="gara hidden">${item.gara_rb || ''}</td>
-                                <td class="gv hidden">${item.gv_rb || ''}</td>
-                                <td class="totale">${item.tot_rb || ''}</td>
-                                <td class="totalone">${item.totale || ''}</td>
+
+        data.forEach((item) => {
+          html += `<tr>
+                                <td>${item.posizione || ""}</td>
+                                <td>${item.id_psn || ""}</td>
+                                <td>${item.id_gt7 || ""}</td>
+                                <td class="pole hidden">${
+                                  item.pole_wg || ""
+                                }</td>
+                                <td class="gara hidden">${
+                                  item.gara_wg || ""
+                                }</td>
+                                <td class="gv hidden">${item.gv_wg || ""}</td>
+                                <td class="totale">${item.tot_wg || ""}</td>
+                                <td class="pole hidden">${
+                                  item.pole_atl || ""
+                                }</td>
+                                <td class="gara hidden">${
+                                  item.gara_atl || ""
+                                }</td>
+                                <td class="gv hidden">${item.gv_atl || ""}</td>
+                                <td class="totale">${item.tot_atl || ""}</td>
+                                <td class="pole hidden">${
+                                  item.pole_fuj || ""
+                                }</td>
+                                <td class="gara hidden">${
+                                  item.gara_fuj || ""
+                                }</td>
+                                <td class="gv hidden">${item.gv_fuj || ""}</td>
+                                <td class="totale">${item.tot_fuj || ""}</td>
+                                <td class="pole hidden">${
+                                  item.pole_aut || ""
+                                }</td>
+                                <td class="gara hidden">${
+                                  item.gara_aut || ""
+                                }</td>
+                                <td class="gv hidden">${item.gv_aut || ""}</td>
+                                <td class="totale">${item.tot_aut || ""}</td>
+                                <td class="pole hidden">${
+                                  item.pole_spa || ""
+                                }</td>
+                                <td class="gara hidden">${
+                                  item.gara_spa || ""
+                                }</td>
+                                <td class="gv hidden">${item.gv_spa || ""}</td>
+                                <td class="totale">${item.tot_spa || ""}</td>
+                                <td class="pole hidden">${
+                                  item.pole_rb || ""
+                                }</td>
+                                <td class="gara hidden">${
+                                  item.gara_rb || ""
+                                }</td>
+                                <td class="gv hidden">${item.gv_rb || ""}</td>
+                                <td class="totale">${item.tot_rb || ""}</td>
+                                <td class="totalone">${item.totale || ""}</td>
                             </tr>`;
-                });
+        });
 
-                html += `</tbody></table></div>`; // Chiudi il contenitore scrollabile
-                container.innerHTML = html;
+        html += `</tbody></table></div>`; // Chiudi il contenitore scrollabile
+        container.innerHTML = html;
 
-                // Aggiungi l'evento click al pulsante per nascondere/mostrare le colonne
-                const toggleColumnsButton = document.getElementById(`toggleColumnsButton${index + 1}`);
-                toggleColumnsButton.addEventListener('click', function() {
-                    const table = document.getElementById(`table${index + 1}`);
-                    const columnsToToggle = table.querySelectorAll('.pole, .gara, .gv');
-                    columnsToToggle.forEach(column => {
-                        column.classList.toggle('hidden');
-                    });
-                    // Verifica lo stato di una colonna specifica per determinare il testo del pulsante
-                    const isHidden = table.querySelector('.pole.hidden');
-                    const buttonText = isHidden ? 'Mostra colonne dei punteggi dettagliati' : 'Nascondi colonne dei punteggi dettagliati';
-                    toggleColumnsButton.textContent = buttonText;
-                });
-
-            })
-            .catch(error => {
-                console.error('Errore nel caricamento della classifica:', error);
-                let container = document.getElementById(`classifica${index + 1}`);
-                container.innerHTML = `<p>Errore nel caricamento della classifica.</p>`;
-            });
-    });
+        // Aggiungi l'evento click al pulsante per nascondere/mostrare le colonne
+        const toggleColumnsButton = document.getElementById(
+          `toggleColumnsButton${index + 1}`
+        );
+        toggleColumnsButton.addEventListener("click", function () {
+          const table = document.getElementById(`table${index + 1}`);
+          const columnsToToggle = table.querySelectorAll(".pole, .gara, .gv");
+          columnsToToggle.forEach((column) => {
+            column.classList.toggle("hidden");
+          });
+          // Verifica lo stato di una colonna specifica per determinare il testo del pulsante
+          const isHidden = table.querySelector(".pole.hidden");
+          const buttonText = isHidden
+            ? "Mostra colonne dei punteggi dettagliati"
+            : "Nascondi colonne dei punteggi dettagliati";
+          toggleColumnsButton.textContent = buttonText;
+        });
+      })
+      .catch((error) => {
+        console.error("Errore nel caricamento della classifica:", error);
+        let container = document.getElementById(`classifica${index + 1}`);
+        container.innerHTML = `<p>Errore nel caricamento della classifica.</p>`;
+      });
+  });
 });
